0f877120e64ce56199510ec97849eae3e0f1f059
KusumaAkshaya <sriramaabhayam@gmail.com>
2024-06-30 18:53:08 +0530

    added three flags

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-function func(){
-   var x= document.getElementById("num").value;
-    document.getElementById("para").style.fontSize = "50px";
-    document.getElementById("para").innerHTML = "Hello " + x ;
-    document.getElementById("para").style.color = "red";
-    document.getElementById("num").value="";
-    alert("hello "+ x)
+function brazilFunc(){
+    var c = "This is the Brazil Flag";
+    document.getElementById("brazil").innerHTML= c;
+    document.getElementById("brazil").style.color = "white";
 }
 
-function table(){
-    var n = document.getElementById("num").value;
-    var arr=[] 
-    for( var i=1; i<=10; i++){
-        arr.push( n + "X" + i  + "=" + i*n );
-        }
-    document.getElementById("para").innerHTML = arr;    
+function outBrazil() {
+    var c="";
+    document.getElementById("brazil").innerHTML= c;
 }
 
-function oddOrEven(){
-    var n = document.getElementById("num").value;
-    if(n%2) {
-        document.getElementById("para").innerHTML = n + " is odd";
-        document.getElementById("para").style.color = "RED";
-        document.getElementById("num").value="";
-        document.getElementById("para").style.fontSize = "29px";
-    } 
-    else{
-        document.getElementById("num").value="";
-        document.getElementById("para").innerHTML = n + " is even";
-        document.getElementById("para").style.color = "blue";
-        document.getElementById("para").style.fontSize = "29px";
-    }
+function indiaFunc(){
+    var c = "This is the India Flag";
+    document.getElementById("india").innerHTML= c;
+    document.getElementById("india").style.color = "white";
+}
+
+function outIndia() {
+    var c="";
+    document.getElementById("india").innerHTML= c;
+}
+
+function usaFunc(){
+    var c = "This is the USA Flag";
+    document.getElementById("usa").innerHTML= c;
+    document.getElementById("usa").style.color = "white";
+}
+
+function outUsa() {
+    var c="";
+    document.getElementById("usa").innerHTML= c;
 }
