72f8f447641c9831fe9ad7b34f2805f1cd841a3b
KusumaAkshaya <sriramaabhayam@gmail.com>
2024-06-30 18:02:38 +0530

    lets start the project

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ function func(){
 
 function table(){
     var n = document.getElementById("num").value;
+    var arr=[] 
     for( var i=1; i<=10; i++){
-        document.getElementById("para").innerHTML = n + "X" + i  + "=" + i*n ;
+        arr.push( n + "X" + i  + "=" + i*n );
         }
+    document.getElementById("para").innerHTML = arr;    
+}
+
+function oddOrEven(){
+    var n = document.getElementById("num").value;
+    if(n%2) {
+        document.getElementById("para").innerHTML = n + " is odd";
+        document.getElementById("para").style.color = "RED";
+        document.getElementById("num").value="";
+        document.getElementById("para").style.fontSize = "29px";
+    } 
+    else{
+        document.getElementById("num").value="";
+        document.getElementById("para").innerHTML = n + " is even";
+        document.getElementById("para").style.color = "blue";
+        document.getElementById("para").style.fontSize = "29px";
+    }
 }
